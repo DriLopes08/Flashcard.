8ecd2192b53cee173658ad50d86afb02168c6c8f
DriLopes08 <141828047+DriLopes08@users.noreply.github.com>
2024-10-25 08:41:00 -0300

    Add files via upload

---
+++ Driele 2/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Em que ano começou a Segunda Guerra Mundial?',
+    'Em 1939'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o maior órgão do corpo humano?',
+    'A pele'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano da Terra?',
+    'O Oceano Pacífico'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o único número par que é primo?',
+    'O número 2'
+)
